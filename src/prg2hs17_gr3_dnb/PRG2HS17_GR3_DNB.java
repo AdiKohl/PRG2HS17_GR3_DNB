3689531a7c5b9de0a9d91121316fed4c8953bd68
AdiKohl <adrian.kohler@stud.hslu.ch>
2017-11-24 10:48:11 +0100

    init commit

---
+++ src/prg2hs17_gr3_dnb/PRG2HS17_GR3_DNB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prg2hs17_gr3_dnb;
+
+/**
+ *
+ * @author Adi
+ */
+public class PRG2HS17_GR3_DNB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
