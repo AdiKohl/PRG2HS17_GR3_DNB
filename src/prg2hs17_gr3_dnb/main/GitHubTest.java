921b124a0310649845105df86921b54085b86e3a
LEGhslu <lucien.egloff@stud.hslu.ch>
2017-12-01 11:15:19 +0100

    Das isch de Wahrheit!

--- src/prg2hs17_gr3_dnb/main/GitHubTest.java
+++ src/prg2hs17_gr3_dnb/main/GitHubTest.java
@@ -14,6 +14,7 @@ public class GitHubTest {
     public GitHubTest() {
         System.out.println("TestGit");
         System.out.println("Hallo");
+        System.out.println("De Adi isch de gröscht!");
     }
     
     
